4a46e365799630f210f76f29fea44094fe38614a
Kechmade <ds.hamzahasnaoui@gmail.com>
2017-07-10 18:18:43 +0200

    Update clusterForce
    - Source and target nodes.
    - Problem of data nesting solved/
    - TO DO : Links ( SVG lines ) …

--- html/charts/clusterForce.js
+++ html/charts/clusterForce.js
@@ -2,51 +2,49 @@
 
     var nodes = raw.model();
 
-    var cluster = nodes.dimension()
-        .title("Clusters")
-        .required(1);
-
-    var size = nodes.dimension()
-        .title("Size")
-        .types(Number)
-
-    var label = nodes.dimension()
-        .title("Label")
-
-    var color = nodes.dimension()
-        .title("Color")
+	var source = nodes.dimension()
+		.title("Source")
+		.required(1);
+		
+	var target = nodes.dimension()
+		.title("Target")
+		.required(1);
 
     nodes.map(function (data){
+    	
+    	var sourceNode = data.map(function(d) {
+    		return {   
+    				sourceNode : source(d),
+    				type  : 'sourceNode'
+    				} ; 
+    		
+    	});
+    	var targetNode = data.map(function(d) {
+    		return {   
+    				targetNode : target(d), 
+    				type  : 'targetNode'
+    				} ; 	
+    	});
+    		    	
+    	var myRelations = data.map(function(d) {
+    		return {
+    				type : 'link',
+    				source : source (d),
+    				target : target(d),
+			
+			}
+			
+		});	
+    		
+    	var myNodes = Array.from(new Set(sourceNode.concat(targetNode).map(JSON.stringify))).map(JSON.parse);
+    
+   	    return myNodes.concat(myRelations);
 
-        var nodeClusters = d3.nest()
-            .key(function (d) { return cluster(d); })
-            .rollup(function (d){
-                return {
-                    type: 'cluster',
-                    cluster: cluster(d[0]),
-                    size: 0,
-                }
-            })
-            .map(data);
-
-        var nodeElements = data.map(function (d) {
-          return {
-                type : 'node',
-                label : label(d),
-                cluster: cluster(d),
-                clusterObject : nodeClusters[cluster(d)],
-                size: size() ? +size(d) : 1,
-                color: color(d)
-            };
-        });
-
-        return nodeElements.concat(d3.values(nodeClusters));
-
-    })
-
+    
+     })
 
     var chart = raw.chart()
-        .title('Clustered Force Layout')
+        .title('Test2')
         .description(
             "Nested circles allow to represent hierarchies and compare values. This visualization is particularly effective to show the proportion between elements through their areas and their position inside a hierarchical structure. <br/>Based on <a href='http://bl.ocks.org/mbostock/7882658'>http://bl.ocks.org/mbostock/7882658</a>")
         .thumbnail("imgs/clusterForce.png")
@@ -61,38 +59,27 @@
     var height = chart.number()
         .title("Height")
         .defaultValue(500)
+        
+    var radius = chart.number()
+        .title("Radius")
+        .defaultValue(6)
 
-    var nodePadding = chart.number()
-        .title("node padding")
-        .defaultValue(2)
-
-    var clusterPadding = chart.number()
-        .title("cluster padding")
-        .defaultValue(10)
-
-    var colors = chart.color()
-         .title("Color scale")
 
     chart.draw(function (selection, data){
 
-        d3.layout.pack()
+    d3.layout.pack()
             .sort(null)
             .size([+width(), +height()])
-            .padding(d3.max([nodePadding(),clusterPadding()]))
+            .padding(d3.max([2,10]))
             .children(function (d) { return d.values; })
             .value(function (d) { return +d.size; })
-            .nodes({
-                values: d3.nest()
-                    .key(function (d) { return d.cluster; })
-                    .entries(data)
-                }
-            );
+            .nodes(data);
 
     var force = d3.layout.force()
         .nodes(data)
         .size([+width(), +height()])
-        .gravity(.01)
-        .charge(0)
+        .gravity(.02)
+        .charge(-10)
         .on("tick", tick)
         .on("end", function(){
           chart.dispatchEndDrawing()
@@ -103,91 +90,48 @@
         .attr("width", width)
         .attr("height", height);
 
-    colors.domain(data.filter(function (d){ return d.type == "node"; }), function (d){ return d.color; });
 
-    var node = g.selectAll("circle")
-        .data(data.filter(function (d){ return d.type == "node"; }))
+//     var link = g.selectAll("line")
+//         .data(data.filter(function (d){ return d.type == "link"; }))
+//         .enter().append("line")
+//             .style("stroke-width", "2")
+//             .style("stroke", "grey")
+//             .call(force.drag); 
+    
+    var nodeSource = g.selectAll("circle")
+        .data(data.filter(function (d){ return d.type == "sourceNode"; }))
         .enter().append("circle")
-            .style("fill", function(d) { return d.color ? colors()(d.color) : colors()(null); })
+        .attr("r", 5)
+        .style("fill", "blue")
             .call(force.drag);
-
-    node.transition()
-        .delay(function(d, i) { return i * 5; })
-        .attrTween("r", function(d) {
-            var i = d3.interpolate(0, +d.r);
-            return function(t) { return d.radius = i(t); };
-        });
-
-    var text = g.selectAll("text")
-        .data(data.filter(function (d){ return d.type == "node"; }))
-        .enter().append("text")
-            .text(function (d){ return d.label; })
-            .attr("text-anchor", "middle")
-            .attr("dy","4")
-            .style("font-size","11px")
-            .style("font-family","Arial, Helvetica")
+            
+     var nodeTarget = g.selectAll("rect")
+        .data(data.filter(function (d){ return d.type == "targetNode"; }))
+        .enter().append("rect")
+        .attr("width", 10)
+        .attr("height", 10)
+        .style("fill", "green")
             .call(force.drag);
-
+            
     function tick(e) {
-        node
-          .each(cluster(10 * e.alpha * e.alpha))
-          .each(collide(.5))
-          .attr("cx", function(d) { return d.x; })
-          .attr("cy", function(d) { return d.y; });
-        text
-          .each(cluster(10 * e.alpha * e.alpha))
-          .each(collide(.5))
-          .attr("x", function(d) { return d.x; })
-          .attr("y", function(d) { return d.y; });
+        nodeSource
+          .attr("cx", function(d) { return d.x = Math.max(radius(), Math.min(width() - radius(), d.x)); })
+          .attr("cy", function(d) { return d.y = Math.max(radius(), Math.min(height() - radius(), d.y)); });
+       
+        nodeTarget
+          .attr("x", function(d) { return d.x = Math.max(radius(), Math.min(width() - radius(), d.x)); })
+          .attr("y", function(d) { return d.y = Math.max(radius(), Math.min(height() - radius(), d.y)); });
+          
+    //     link
+//           .attr("x1", function(d) { return d.sourceNode.x; })
+//           .attr("y1", function(d) { return d.sourceNode.y; })	
+//           .attr("x2", function(d) { return d.targetNode.x; })
+//           .attr("y2", function(d) { return d.targetNode.x; });
+		
+		     chart.dispatchStartDrawing()
 
-        chart.dispatchStartDrawing()
     }
-
-    function cluster(alpha) {
-        return function(d) {
-            if (d.type != "node") return;
-            var cluster = d.clusterObject;
-            var x = d.x - cluster.x,
-                y = d.y - cluster.y,
-                l = Math.sqrt(x * x + y * y),
-                r = d.r + cluster.r;
-            if (l != r) {
-              l = (l - r) / l * alpha;
-              d.x -= x *= l;
-              d.y -= y *= l;
-              cluster.x += x;
-              cluster.y += y;
-            }
-        };
-    }
-
-    function collide(alpha) {
-        var quadtree = d3.geom.quadtree(data);
-        return function(d) {
-            var r = d.r + Math.max(+nodePadding(), +clusterPadding()),
-                nx1 = d.x - r,
-                nx2 = d.x + r,
-                ny1 = d.y - r,
-                ny2 = d.y + r;
-
-            quadtree.visit(function(quad, x1, y1, x2, y2) {
-                if (quad.point && (quad.point !== d)) {
-                    var x = d.x - quad.point.x,
-                        y = d.y - quad.point.y,
-                        l = Math.sqrt(x * x + y * y),
-                        r = d.r + quad.point.radius + (d.cluster === quad.point.cluster ? +nodePadding() : +clusterPadding());
-                    if (l < r) {
-                      l = (l - r) / l * alpha;
-                      d.x -= x *= l;
-                      d.y -= y *= l;
-                      quad.point.x += x;
-                      quad.point.y += y;
-                    }
-                }
-                return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
-            });
-        };
-    }
+    
 
   })
 
